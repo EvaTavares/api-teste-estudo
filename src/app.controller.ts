94f51059d35658ddb8a5d60f7d24dd30be267e4f
Eva Tavares <eva.programadora@gmail.com>
2024-02-21 12:00:06 -0300

    add: sub rota

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// rota
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // sub-rota
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // tbm pode passar parametros
+  // @Get('hello/:id')
+  // getHello(@Param() params: any): string {
+  //   return this.appService.getHello();
+  // }
 }
